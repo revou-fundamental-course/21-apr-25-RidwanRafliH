c7559fb6521c64b84de5284fbdd2c4cd5de23f50
RidwanRafliH <ridwanrafli735@gmail.com>
2025-04-28 17:48:31 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const celsiusInput = document.getElementById('celsius');
+    const fahrenheitInput = document.getElementById('fahrenheit');
+    const calculationSteps = document.getElementById('calculation-steps');
+    const convertBtn = document.getElementById('convert-btn');
+    const resetBtn = document.getElementById('reset-btn');
+
+    fahrenheitInput.setAttribute('readonly', true);
+});
