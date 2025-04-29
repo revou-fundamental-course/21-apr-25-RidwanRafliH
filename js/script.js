74304fe19e379835c1b75085f2cfd665614fb6b3
RidwanRafliH <ridwanrafli735@gmail.com>
2025-04-29 18:06:32 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { 
     const celsiusInput = document.getElementById('celsius');
     const fahrenheitInput = document.getElementById('fahrenheit');
     const calculationSteps = document.getElementById('calculation-steps');
@@ -6,4 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const resetBtn = document.getElementById('reset-btn');
 
     fahrenheitInput.setAttribute('readonly', true);
+
+    convertBtn.addEventListener('click', () => {
+        const celsius = parseFloat(celsiusInput.value);
+
+        if (isNaN(celsius)) {
+            fahrenheitInput.value = '';
+            calculationSteps.value = 'Masukkan angka yang valid untuk dikonversi.';
+            return;
+        }
+
+        const fahrenheit = (celsius * 9/5) + 32;
+
+        fahrenheitInput.value = fahrenheit.toFixed(2);
+        calculationSteps.value = `${celsius}°C * (9/5) + 32 = ${fahrenheit.toFixed(2)}°F`;
+    });
+
+    resetBtn.addEventListener('click', () => {
+        celsiusInput.value = '';
+        fahrenheitInput.value = '';
+        calculationSteps.value = '';
+    });
 });
